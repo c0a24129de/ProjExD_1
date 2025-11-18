9cfe5e61ee52329a606bb4bd4bfacb1fee9eb922
c0a24129de <c0a24129de@edu.teu.ac.jp>
2025-11-18 18:02:55 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -34,7 +34,8 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
+            kk_rct.move_ip((2, 0))
+        kk_rct.move_ip((-1, 0))
         screen.blit(kk_img,kk_rct) #練習4 10
         pg.display.update()
         tmr += 1        
